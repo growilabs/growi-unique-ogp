90acd391e186a4151879477566ff9e820f557f12
yuto-o <yuto-o@weseek.co.jp>
2022-01-20 03:35:22 +0000

    overwrite babel eslint parser by @babel/eslint-parser and this works properly

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,22 +1,9 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+  },
   extends: [
     'weseek',
-    'weseek/typescript',
   ],
-  env: {
-    node: true,
-  },
-  rules: {
-    'import/prefer-default-export': 'off',
-    indent: [
-      'error',
-      2,
-      {
-        SwitchCase: 1,
-        ArrayExpression: 'first',
-        FunctionDeclaration: { body: 1, parameters: 2 },
-        FunctionExpression: { body: 1, parameters: 2 },
-      },
-    ],
-  },
 };
